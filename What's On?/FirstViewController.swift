c16660d649f65fa442e1d8c0d75eed16af49eaf8
Paul Makuszewski <pmakusze@kent.edu>
2015-03-13 20:32:55 -0400

    Initial commit from terminal

---
+++ What's On?/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  What's On?
+//
+//  Created by Paul Makuszewski on 3/13/15.
+//  Copyright (c) 2015 Makken. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
