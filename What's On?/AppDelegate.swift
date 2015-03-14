c460391447e7e25457c354ff2ae9c55d82683aad
Paul Makuszewski <pmakusze@kent.edu>
2015-03-13 20:55:37 -0400

    Xcode Commit initial

--- What's On?/AppDelegate.swift
+++ What's On?/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  What's On?
 //
-//  Created by Paul Makuszewski on 3/13/15.
+//  Created by Paul Makuszewski and Aaron Kenyon on 3/13/15.
 //  Copyright (c) 2015 Makken. All rights reserved.
 //
 
